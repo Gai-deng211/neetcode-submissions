39edca0d7bca7eadf744a5221af001495a35f3ee
Gai Deng <gai.gatluak.deng@gmail.com>
2026-07-12 18:06:07 -0700

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+
+        def climb(i):
+            if i <= 1:
+                return 1
+            if i not in memo:
+                memo[i] = climb(i - 1) + climb(i - 2)
+            return memo[i]
+
+        return climb(n)
